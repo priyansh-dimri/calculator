6a30804ce561362713434728e024bd6fcf4591ce
Priyansh Dimri <priyanshd0908@gmail.com>
2024-11-06 18:03:30 +0530

    chore: format entire script.js using auto-format shortcut key

--- script.js
+++ script.js
@@ -2,44 +2,42 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => {
-  if(num2 === 0) return 'LOL';
+  if (num2 === 0) return "LOL";
   return num1 / num2;
 };
 
-let num1 , num2 , operator, number_to_be_added = 1;
+let num1,
+  num2,
+  operator,
+  number_to_be_added = 1;
 
 // Maps numeric button ids to the value of the button
 const numeric_button_values = {
-  'zero': '0',
-  'one': '1',
-  'two': '2',
-  'three': '3',
-  'four': '4',
-  'five': '5',
-  'six': '6',
-  'seven': '7',
-  'eight': '8',
-  'nine': '9',
+  zero: "0",
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
 };
 
 // Maps operator button ids to the character equivalent of the operator
 const operator_button_values = {
-  'add': '+',
-  'subtract': '-',
-  'multiply': '*',
-  'divide': '/',
-}
+  add: "+",
+  subtract: "-",
+  multiply: "*",
+  divide: "/",
+};
 
 // Array of supported operators of the calculator
-const supported_operators = [
-  '+',
-  '-',
-  '*',
-  '/',
-]
+const supported_operators = ["+", "-", "*", "/"];
 
 const operate = (num1, num2, operator) => {
-  if(isNaN(num1) || isNaN(num2)) return 'LOL';
+  if (isNaN(num1) || isNaN(num2)) return "LOL";
   num1 = Number(num1);
   num2 = Number(num2);
 
@@ -65,151 +63,150 @@ const operate = (num1, num2, operator) => {
   return result.toString();
 };
 
-const inputButtons = document.querySelectorAll('.input');
-const calculatorDisplay = document.querySelector('#calculator-display');
+const inputButtons = document.querySelectorAll(".input");
+const calculatorDisplay = document.querySelector("#calculator-display");
 
 const modifyCurrentNumber = (toBeAdded) => {
-  if(number_to_be_added === 1){
-    if(num1 && exceedsDecimalLimit(num1)) return;
-    if(!num1) num1 = "";
+  if (number_to_be_added === 1) {
+    if (num1 && exceedsDecimalLimit(num1)) return;
+    if (!num1) num1 = "";
     num1 += toBeAdded;
     modifyCalculatorDisplay(num1);
-  }
-  else{
-    if(num2 && exceedsDecimalLimit(num2)) return;
-    if(!num2) num2 = "";
+  } else {
+    if (num2 && exceedsDecimalLimit(num2)) return;
+    if (!num2) num2 = "";
     num2 += toBeAdded;
     modifyCalculatorDisplay(num2);
   }
-}
+};
 
 const addDecimalToCurrentNumber = () => {
-  if(number_to_be_added === 1){
-    if(!num1) num1 = "0";
-    if(num1.includes('.')) return;
-    num1 += '.';
+  if (number_to_be_added === 1) {
+    if (!num1) num1 = "0";
+    if (num1.includes(".")) return;
+    num1 += ".";
     modifyCalculatorDisplay(num1);
-  }
-  else{
-    if(!num2) num2 = "0";
-    if(num2.includes('.')) return;
-    num2 += '.';
+  } else {
+    if (!num2) num2 = "0";
+    if (num2.includes(".")) return;
+    num2 += ".";
     modifyCalculatorDisplay(num2);
   }
-}
+};
 
 const exceedsDecimalLimit = (toDisplay) => {
-  if(!toDisplay.includes('.')) return false;
+  if (!toDisplay.includes(".")) return false;
 
-  if(toDisplay.split('.')[1].length > 4) return true;
+  if (toDisplay.split(".")[1].length > 4) return true;
   return false;
-}
+};
 
 const roundDisplayNumber = (toDisplay) => {
-  if(isNaN(toDisplay)) return toDisplay;
+  if (isNaN(toDisplay)) return toDisplay;
 
-  if(!exceedsDecimalLimit(toDisplay)) return toDisplay;
+  if (!exceedsDecimalLimit(toDisplay)) return toDisplay;
 
   let display_num = Number(toDisplay);
   return display_num.toFixed(5).toString();
-}
+};
 
 const modifyCalculatorDisplay = (toDisplay) => {
   calculatorDisplay.textContent = roundDisplayNumber(toDisplay);
-}
+};
 
 const inputsValid = () => {
-  return (num1 !== undefined) && (num2 !== undefined) && (operator !== undefined);
-}
+  return num1 !== undefined && num2 !== undefined && operator !== undefined;
+};
 
 const handleAllClearClick = () => {
   num1 = num2 = operator = undefined;
   number_to_be_added = 1;
 
-  modifyCalculatorDisplay('');
-}
+  modifyCalculatorDisplay("");
+};
 
 const handleBackspaceClick = () => {
-  if(number_to_be_added === 1){
-    if(num1) {
+  if (number_to_be_added === 1) {
+    if (num1) {
       num1 = num1.slice(0, -1);
       modifyCalculatorDisplay(num1);
     }
-  }
-  else{
-    if(num2) {
+  } else {
+    if (num2) {
       num2 = num2.slice(0, -1);
-      if(num2 === "") num2 = undefined;
+      if (num2 === "") num2 = undefined;
       modifyCalculatorDisplay(num2);
-    }
-    else {
+    } else {
       operator = undefined;
       number_to_be_added = 1;
       modifyCalculatorDisplay(num1);
     }
   }
-}
+};
 
 const handleEqualsClick = () => {
-  if(inputsValid()) {
+  if (inputsValid()) {
     let result = operate(num1, num2, operator);
     modifyCalculatorDisplay(result);
 
-    if(result === 'LOL') {
+    if (result === "LOL") {
       num1 = undefined;
-    }
-    else {
+    } else {
       num1 = result;
     }
 
     number_to_be_added = 1;
     num2 = operator = undefined;
   }
-}
+};
 
 const handleOperatorClick = (operator_clicked) => {
-  if(inputsValid()) {
+  if (inputsValid()) {
     let result = operate(num1, num2, operator);
     modifyCalculatorDisplay(result);
 
-    if(result === 'LOL') {
+    if (result === "LOL") {
       num1 = num2 = operator = undefined;
       number_to_be_added = 1;
       return;
     }
-    
+
     num1 = result;
     num2 = undefined;
   }
 
-  if(num1 !== undefined){
+  if (num1 !== undefined) {
     operator = operator_clicked;
     number_to_be_added = 2;
   }
-}
+};
 
 const handleButtonClick = (buttonId) => {
-  if(buttonId === 'ac') handleAllClearClick();
-  else if(buttonId === 'decimal') addDecimalToCurrentNumber();
-  else if(buttonId === 'backspace') handleBackspaceClick();
-  else if(buttonId === 'equals') handleEqualsClick();
-  else if(buttonId in numeric_button_values) modifyCurrentNumber(numeric_button_values[buttonId]);
-  else if(buttonId in operator_button_values) handleOperatorClick(operator_button_values[buttonId]);
-}
+  if (buttonId === "ac") handleAllClearClick();
+  else if (buttonId === "decimal") addDecimalToCurrentNumber();
+  else if (buttonId === "backspace") handleBackspaceClick();
+  else if (buttonId === "equals") handleEqualsClick();
+  else if (buttonId in numeric_button_values)
+    modifyCurrentNumber(numeric_button_values[buttonId]);
+  else if (buttonId in operator_button_values)
+    handleOperatorClick(operator_button_values[buttonId]);
+};
 
 inputButtons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     handleButtonClick(button.id);
-  })
-})
+  });
+});
 
 // Keyboard support
 document.addEventListener("keydown", (e) => {
   let pressed_key = e.key;
 
-  if(pressed_key !== ' ' && !isNaN(pressed_key)) modifyCurrentNumber(pressed_key);
-  else if(pressed_key === 'Backspace') handleBackspaceClick();
-  else if(pressed_key === 'Enter') handleEqualsClick();
-  else if(pressed_key === '.') addDecimalToCurrentNumber();
-  else if(supported_operators.includes(pressed_key)) handleOperatorClick(pressed_key);
-})
+  if (pressed_key !== " " && !isNaN(pressed_key))
+    modifyCurrentNumber(pressed_key);
+  else if (pressed_key === "Backspace") handleBackspaceClick();
+  else if (pressed_key === "Enter") handleEqualsClick();
+  else if (pressed_key === ".") addDecimalToCurrentNumber();
+  else if (supported_operators.includes(pressed_key))
+    handleOperatorClick(pressed_key);
+});
