1b59bda11f6982e9ffb5c0f2dc6502977fae9851
Priyansh Dimri <priyanshd0908@gmail.com>
2024-11-06 17:51:09 +0530

    feat: add keyboard support to the calculator
    - Created various functions to support modularity
    - Remove unnecessary console.log() statements

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => {
-  console.log(num2);
   if(num2 === 0) return 'LOL';
   return num1 / num2;
 };
@@ -28,6 +27,13 @@ const operator_button_values = {
   'multiply': '*',
   'divide': '/',
 }
+
+const supported_operators = [
+  '+',
+  '-',
+  '*',
+  '/',
+]
 
 const operate = (num1, num2, operator) => {
   if(isNaN(num1) || isNaN(num2)) return 'LOL';
@@ -134,6 +140,44 @@ const handleBackspaceClick = () => {
   }
 }
 
+const handleEqualsClick = () => {
+  if(inputsValid()) {
+    let result = operate(num1, num2, operator);
+    modifyCalculatorDisplay(result);
+
+    if(result === 'LOL') {
+      num1 = undefined;
+    }
+    else {
+      num1 = result;
+    }
+
+    number_to_be_added = 1;
+    num2 = operator = undefined;
+  }
+}
+
+const handleOperatorClick = (operator_clicked) => {
+  if(inputsValid()) {
+    let result = operate(num1, num2, operator);
+    modifyCalculatorDisplay(result);
+
+    if(result === 'LOL') {
+      num1 = num2 = operator = undefined;
+      number_to_be_added = 1;
+      return;
+    }
+    
+    num1 = result;
+    num2 = undefined;
+  }
+
+  if(num1 !== undefined){
+    operator = operator_clicked;
+    number_to_be_added = 2;
+  }
+}
+
 const handleButtonClick = (buttonId) => {
   if(buttonId === 'ac') {
     num1 = num2 = operator = undefined;
@@ -143,45 +187,13 @@ const handleButtonClick = (buttonId) => {
   }
   else if(buttonId === 'decimal') addDecimalToCurrentNumber();
   else if(buttonId === 'backspace') handleBackspaceClick();
-  else if(buttonId === 'equals') {
-    if(inputsValid()) {
-      let result = operate(num1, num2, operator);
-      modifyCalculatorDisplay(result);
-
-      if(result === 'LOL') {
-        num1 = undefined;
-      }
-      else {
-        num1 = result;
-      }
-
-      number_to_be_added = 1;
-      num2 = operator = undefined;
-    }
-  }
+  else if(buttonId === 'equals') handleEqualsClick();
   else if(buttonId in numeric_button_values) {
     let number_pressed = numeric_button_values[buttonId];
     modifyCurrentNumber(number_pressed);
   }
   else if(buttonId in operator_button_values) {
-    if(inputsValid()) {
-      let result = operate(num1, num2, operator);
-      modifyCalculatorDisplay(result);
-
-      if(result === 'LOL') {
-        num1 = num2 = operator = undefined;
-        number_to_be_added = 1;
-        return;
-      }
-      
-      num1 = result;
-      num2 = undefined;
-    }
-
-    if(num1 !== undefined){
-      operator = operator_button_values[buttonId];
-      number_to_be_added = 2;
-    }
+    handleOperatorClick(operator_button_values[buttonId]);
   }
 }
 
@@ -189,4 +201,25 @@ inputButtons.forEach((button) => {
   button.addEventListener('click', () => {
     handleButtonClick(button.id);
   })
+})
+
+// Keyboard support
+document.addEventListener("keydown", (e) => {
+  let pressed_key = e.key;
+
+  if(pressed_key !== ' ' && !isNaN(pressed_key)){ 
+    modifyCurrentNumber(pressed_key);
+  }
+  else if(pressed_key === 'Backspace') {
+    handleBackspaceClick();
+  }
+  else if(pressed_key === 'Enter') {
+    handleEqualsClick();
+  }
+  else if(pressed_key === '.') {
+    addDecimalToCurrentNumber();
+  }
+  else if(supported_operators.includes(pressed_key)) {
+    handleOperatorClick(pressed_key);
+  }
 })
